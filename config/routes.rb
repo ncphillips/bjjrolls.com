49815e8929dc6ebde94d8bbd1f85eb8aa9b6a5c5
Nolan Phillips <nolan@ncphi.com>
2022-06-29 22:09:14 -0300

    User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html

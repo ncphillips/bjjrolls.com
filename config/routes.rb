0bf15bfce20ba89a5809d9939b7cb96b5006d7d2
Nolan Phillips <nolan@ncphi.com>
2022-07-02 08:12:40 -0300

    Remove rails code

--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  devise_for :users
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-end
